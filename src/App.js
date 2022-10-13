305b4d1d5be1059e485c8503d78ddd40c8377449
Prosper <nkonyeprospero@gmail.com>
2022-10-13 04:09:19 +0100

    first  time done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import video from "../src/img/background.mp4"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  <div className="App">
+
+      <div className="App-container">
+
+            <video loop autoPlay id="background-video">
+              <source
+                src={video}
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+
+      </div>
+
+
+
+     <div className="body-container">
+
+
+            <div className="divone">
+
+                <div className="connectwallet">
+                    Connect <br />
+                     <div className='red'>Button</div>
+                </div>
+
+            </div>
+
+
+
+            <div className="divtwo">
+            
+
+                <div className="contractbalance">0.2Eth</div>
+
+                <div className="totalminted">500/1000 Minted</div>
+
+                <div className="mintbutton">
+                   Mint <br />
+                   <div className='redtwo'>Button</div>
+                </div>
+              
+
+            </div>
+
+
+
+        </div>
+
+
+
+
     </div>
   );
 }
